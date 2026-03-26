da332add3d0936c25b2f5376873542def5664ea4
RhondaD09 <rhondadavisl@outlook.com>
2026-03-26 14:49:16 -0400

    Initial Commit

---
+++ Resonance/ResonanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResonanceApp.swift
+//  Resonance
+//
+//  Created by Rhonda Davis on 3/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ResonanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ResonanceTests/ResonanceTests.swift
@@ -0,0 +1,19 @@
+//
+//  ResonanceTests.swift
+//  ResonanceTests
+//
+//  Created by Rhonda Davis on 3/26/26.
+//
+
+import Testing
+@testable import Resonance
+
+struct ResonanceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
